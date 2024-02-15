84f7cd06e8161749cd8b7123d6ce707e59fd6f5d
Weerawut Chaiyasomboon <weerawut@live.com>
2024-02-15 12:43:52 +0700

    Initial Commit

---
+++ DataFlow/DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp.swift
+//  DataFlow
+//
+//  Created by Weerawut Chaiyasomboon on 15/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
